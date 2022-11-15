c2a9a45537bb768cc29e8ceebe16341409be5f62
unknown <cchisabasa@ucentral.edu.co>
2022-11-14 20:37:36 -0500

    proyecto se subio

---
+++ php/conexion_be.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$conexion = mysql_connect("local host", " root", "", "login_register_db");
+/*
+  if($conexion){
+    echo 'conectado exitosamente a la base de datos';
+  }else{
+    echo 'no se ha podido conectar a la base de datos';
+  }
+  */
+?>
